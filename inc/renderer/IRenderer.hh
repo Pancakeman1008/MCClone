863b04d22c13e0e2b1805ed23bc40f565896e86b
Noah Willingham <noahwillingham5@gmail.com>
2022-12-03 19:37:34 +0000

    add = 0 i forgor

--- inc/renderer/IRenderer.hh
+++ inc/renderer/IRenderer.hh
@@ -7,9 +7,9 @@ class IRenderer {
 public:
     virtual ~IRenderer() = default;
 
-    virtual auto shouldExit() -> bool;
-    virtual auto draw() -> void;
+    virtual auto shouldExit() -> bool = 0;
+    virtual auto draw() -> void = 0;
 
-    virtual auto updateCameraPosition(Position newPos) -> void;
-    virtual auto updateChunk(const Chunk& chunk) -> void;
+    virtual auto updateCameraPosition(Position newPos) -> void = 0;
+    virtual auto updateChunk(const Chunk& chunk) -> void = 0;
 };
